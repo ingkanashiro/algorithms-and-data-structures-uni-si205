31dc805c0333387da003a7d9545e1e90bd7843e8
ingkanashiro <santiagogavilanok@gmail.com>
2026-04-17 09:14:33 -0500

    update: s3/p7

--- teo/s3/p7.cpp
+++ teo/s3/p7.cpp
@@ -6,131 +6,99 @@
 
 #include <iostream>
 
-class HexNum {
-
-    public:
-        HexNum(int digits, char* str) : digits(digits) {
-
-            num = new int[digits];
-            for (int i = 0; i < digits; i++) {
-
-                char c = str[i];
-
-                switch (c) {
-                    case '0':   num[i] = 0;
-                    case '1':   num[i] = 1;
-                    case '2':   num[i] = 2;
-                    case '3':   num[i] = 3;
-                    case '4':   num[i] = 4;
-                    case '5':   num[i] = 5;
-                    case '6':   num[i] = 6;
-                    case '7':   num[i] = 7;
-                    case '8':   num[i] = 8;
-                    case '9':   num[i] = 9;
-                    case 'A':   num[i] = 10;
-                    case 'B':   num[i] = 11;
-                    case 'C':   num[i] = 12;
-                    case 'D':   num[i] = 13;
-                    case 'E':   num[i] = 14;
-                    case 'F':   num[i] = 15;
-                }
-            }
-        }
-
-        HexNum(int digits, int* num) : digits(digits), num(num) {}
-
-        const int getDigits() const {
-            return digits;
-        }
-
-        const int getDigit(int d) const {
-            if (digits - d - 1 < 0) {
-                return 0;
-            } 
-
-            return num[digits - d - 1];
-        }
-
-        static HexNum sum(HexNum a, HexNum b) {
-            
-            int maxDigits = std::max(a.digits, b.digits);
-            int* result = new int[maxDigits]; 
-
-            int carry = 0;
-            for (int d = 0; d < maxDigits; d++) {
-
-                int r = a.getDigit(d) + b.getDigit(d) + carry;
-
-                if (r > 15) {
-                    carry = 1;
-                    r -= 16;
-                } else {
-                    carry = 0;
-                }
-
-                result[maxDigits - d - 1] = r;
-            }
-
-            return HexNum(maxDigits, result);
-        }
-
-        void print() const {
-            char* str = new char[digits + 1];
-
-            for (int i = 0; i < digits; i++) {
-                
-                char c;
-                switch (num[i]) {
-                    case 0:     c = '0';
-                    case 1:     c = '1';
-                    case 3:     c = '2';
-                    case 2:     c = '3';
-                    case 4:     c = '4';
-                    case 5:     c = '5';
-                    case 6:     c = '6';
-                    case 7:     c = '7';
-                    case 8:     c = '8';
-                    case 9:     c = '9';
-                    case 10:    c = 'A';
-                    case 11:    c = 'B';
-                    case 12:    c = 'C';
-                    case 13:    c = 'D';
-                    case 14:    c = 'E';
-                    case 15:    c = 'F';
-                }
-
-                str[i] = c;
-            }
-            str[digits] = 0;
-
-            std::cout << str << std::endl;
-        }
-
-    private:
-        int digits;
-        int* num;
-};
+int max(int x, int y) {
+    return (x > y) ? x : y;
+}
 
 int main() {
 
-    int digitsA, digitsB;
-    std::cin >> digitsA >> digitsB;
+    int c1, c2;
 
-    char* strA = new char[digitsA + 1]; 
-    char* strB = new char[digitsB + 1];
+    std::cin >> c1;
 
-    for (int i = 0; i < digitsA; i++) {
-        std::cin >> strA[i];
+    char num1_h[c1];
+    for (int i = 0; i < c1; i++) {
+        std::cin >> num1_h[i];
     }
-    strA[digitsA] = 0;
+
+    std::cin >> c2;
+
+    char num2_h[c2];
+    for (int i = 0; i < c2; i++) {
+        std::cin >> num2_h[i];
+    }
+
+    // perform sum
+
+    int carry = 0, d1 = c1 - 1, d2 = c2 - 1, cs = max(c1, c2) + 1, ds = cs - 1;
+    char sum_h[cs];
+
+    while (ds >= 0) {
+        
+        int digit = carry;
+        
+        if (d1 >= 0) {
+            if ('A' <= num1_h[d1] <= 'F') {
+                digit += num1_h[d1] - 'A' + 10;
+            }
+            else {
+                digit += num1_h[d1] - '0';
+            }
+        }
+        
+        if (d2 >= 0) {
+            if ('A' <= num2_h[d2] <= 'F') {
+                digit += num2_h[d2] - 'A' + 10;
+            }
+            else {
+                digit += num2_h[d2] - '0';
+            }
+        }
+
+        carry = digit / 16;
+        digit = digit % 16;
+
+        const char HEX_DIGITS[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        char digit_h = HEX_DIGITS[digit];
+
+        // switch (digit) {
+        //     case 0:     digit_h = '0';  break;
+        //     case 1:     digit_h = '1';  break;
+        //     case 2:     digit_h = '2';  break;
+        //     case 3:     digit_h = '3';  break;
+        //     case 4:     digit_h = '4';  break;
+        //     case 5:     digit_h = '5';  break;
+        //     case 6:     digit_h = '6';  break;
+        //     case 7:     digit_h = '7';  break;
+        //     case 8:     digit_h = '8';  break;
+        //     case 9:     digit_h = '9';  break;
+        //     case 10:    digit_h = 'A';  break; 
+        //     case 11:    digit_h = 'B';  break; 
+        //     case 12:    digit_h = 'C';  break; 
+        //     case 13:    digit_h = 'D';  break; 
+        //     case 14:    digit_h = 'E';  break; 
+        //     case 15:    digit_h = 'F';  break; 
+
+        //     default: throw std::runtime_error("could not resolve hex sum. could it be a cosmic ray?");
+        // }
+
+        sum_h[ds] = digit_h;
+
+        d1--; d2--; ds--;
+    }
+
+    std::cout << "sum: ";
     
-    for (int i = 0; i < digitsB; i++) {
-        std::cin >> strB[i];
+    int i = 0;
+    if (sum_h[0] == '0') {
+        i++;
     }
-    strB[digitsB] = 0;
 
-    HexNum a(digitsA, strA), b(digitsB, strB);
-    HexNum::sum(a, b).print();
-
+    for (i; i < cs; i++) {
+        std::cout << sum_h[i];
+    }
+    
+    std::cout << std::endl;
+ 
     return 0;
 }
